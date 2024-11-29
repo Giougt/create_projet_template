32c8d61e72bd6cd70b746076760dc8130920464b
Giougt <154607600+Giougt@users.noreply.github.com>
2024-11-29 11:03:26 +0100

    Update setup_web.sh

--- web/setup_web.sh
+++ web/setup_web.sh
@@ -13,3 +13,24 @@ touch $nom_projet/index.html
 touch $nom_projet/js/script.js
 touch $nom_projet/css/styles.css
 touch $nom_projet/README.md
+
+#complete file 
+#html 
+echo "<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <title>Document</title>
+    <link type=\"stylesheet\" href=\"$nom_projet/css/styles.css\"> 
+</head>
+<body>
+    <script src=\"/js/script.js\"></script>
+</body>
+</html>" >> $nom_projet/index.html
+
+#css
+echo "*{
+margin: 0;
+padding: 0;
+}" >> $nom_projet/css/styles.css
